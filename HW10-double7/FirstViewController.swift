35450776db18046ec3995191f3c1bd3e447f4f9a
TorontoTokyo <acxa20092mail.ru>
2022-05-20 21:14:03 +0600

    FirstViewController

---
+++ HW10-double7/FirstViewController.swift
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.swift
+//  HW10-double7
+//
+//  Created by Асхат Джумамуратов on 20.05.2022.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
